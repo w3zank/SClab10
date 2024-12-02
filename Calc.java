279015a89b959a395d074ff3c70d94c46d02b4a3
Azan Hyder <ahyder.bese21seecs@student.nust.edu.pk>
2024-12-02 02:37:08 -0800

    First push

---
+++ Calc.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Calc {
+
+  public static void main(String[] args) throws Exception {
+    String inputFile = null;
+    if (args.length > 0) inputFile = args[0];
+    InputStream is = System.in;
+    if (inputFile != null) is = new FileInputStream(inputFile);
+    CharStream input = CharStreams.fromStream(is);
+    LabeledExprLexer lexer = new LabeledExprLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    LabeledExprParser parser = new LabeledExprParser(tokens);
+    ParseTree tree = parser.prog(); // parse
+
+    EvalVisitor eval = new EvalVisitor();
+    eval.visit(tree);
+ }
+}
